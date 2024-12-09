f7fd89552732fb00112f703ebf92479f65fd98d2
mrlbni <suchitamiki06@gmail.com>
2024-12-09 07:30:55 +0530

    Update forcesub.py

--- megadl/forcesub.py
+++ megadl/forcesub.py
@@ -46,7 +46,7 @@ async def handle_force_subscribe(bot, message):
     except Exception:
         await bot.send_message(
             chat_id=message.from_user.id,
-            text="Something Went Wrong. Contact My [Support Group](https://t.me/s3yori).",
+            text="Something Went Wrong. Contact My [Support Group](https://t.me/noob_je).",
             parse_mode="markdown",
             disable_web_page_preview=True,
             reply_to_message_id=message.message_id,
