78040214d43ec7e44c3c4223f93d43c5d91c5822
mrlbni <suchitamiki06@gmail.com>
2024-12-09 07:46:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@
 import os
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 123))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", 22419004))
+    API_HASH = os.environ.get("API_HASH", "34982b52c4a83c2af3ce8f4fe12fe4e1")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7817412794:AAGWTNTC92pTqaC0dUrF0dHsQXKJ4ZZzROU")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     TG_MAX_SIZE = 2040108421
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1316963576))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 6742022802))
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 
